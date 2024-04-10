065ce24cc0abb3aefb98b5d87fc5df0ea80cb546
Felipe Marano <felipehenriquemarano@gmail.com>
2024-04-10 11:30:25 -0300

    att cosnt media

--- calculo-media.js
+++ calculo-media.js
@@ -6,7 +6,7 @@
 
 const notas = [10, 6.5, 8, 7.5];
 
-const media = notas [0] + notas [1] + notas [2] + notas [3] / notas.length; // o tamanho da lista (no caso,dividido por 4)
+const media = (notas [0] + notas [1] + notas [2] + notas [3]) / notas.length; // o tamanho da lista (no caso,dividido por 4)
 // mesma coisa que: const media = (nota1 + nota2 + nota3 + nota4) / 4;
 
 console.log(media);
