f5d1532558a6477d99afa21d1481c0732cd52d60
Felipe Marano <felipehenriquemarano@gmail.com>
2024-04-10 11:30:50 -0300

    add elementos

--- adicionar-notas.js
+++ adicionar-notas.js
@@ -1,1 +1,7 @@
 const notas = [10, 6, 8];
+
+notas.push (7); // para colocar o numero 7 dentro da lista
+
+const media = (notas [0] + notas [1] + notas [2] + notas [3]) / notas.length; // o tamanho da lista (no caso,dividido por 4)
+
+console.log(media);
