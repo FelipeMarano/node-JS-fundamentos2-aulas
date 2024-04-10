23100e98dbb195960da04b76227c852ebc62bdbd
Felipe Marano <felipehenriquemarano@gmail.com>
2024-04-10 11:16:55 -0300

    att calculo-media

--- calculo-media.js
+++ calculo-media.js
@@ -1,8 +1,12 @@
-const nota1 = 10;
-const nota2 = 6.5;
-const nota3 = 8;
-const nota4 = 7.5;
+// const nota1 = 10;
+// const nota2 = 6.5;
+// const nota3 = 8;
+// const nota4 = 7.5;
+// é a mesma coisa que:
 
-const media = (nota1 + nota2 + nota3 + nota4) / 4;
+const notas = [10, 6.5, 8, 7.5];
+
+const media = notas [0] + notas [1] + notas [2] + notas [3] / notas.length; // o tamanho da lista (no caso,dividido por 4)
+// mesma coisa que: const media = (nota1 + nota2 + nota3 + nota4) / 4;
 
 console.log(media);
