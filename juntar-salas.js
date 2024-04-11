b9c63d0d94eccc4deaf1b16a13c425dad42ea582
Felipe Marano <felipehenriquemarano@gmail.com>
2024-04-11 13:21:36 -0300

    concatenando arrays

---
+++ juntar-salas.js
@@ -0,0 +1,6 @@
+const salaJS = ['Evaldo', 'Camis', 'Mari'];
+const salaPython = ['Ju', 'Leo', 'Raquel'];
+
+const salasUnificadas = salaJS.concat(salaPython); // concat = para juntar/concatenar os dois arrays 
+
+console.log(salasUnificadas); 
