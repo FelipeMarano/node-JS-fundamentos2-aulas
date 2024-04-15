3ac34580d386c87ba39b4b47539cd70e243e4247
Felipe Marano <felipehenriquemarano@gmail.com>
2024-04-15 13:42:08 -0300

    att forEach.js

--- forEach.js
+++ forEach.js
@@ -1,18 +1,21 @@
+ // callback = funções que chamam outras funções dentro dela
+
 const notas = [10, 6.5, 8, 7.5];
 
 let somaDasNotas = 0;
 
-notas.forEach()(function (nota) { // forEach() = o parâmetro (nota) é uma função que é chamada de volta quando executamos o forEach()
-    somaDasNotas += nota;         // callback = funções que chamam outras funções dentro dela
-})
+ notas.forEach(somaNotas); // forEach() = o parâmetro (nota) é uma função que é chamada de volta quando executamos o forEach()
+
+ function somaNotas (nota) {
+    somaDasNotas += nota;
+}
 
 // ou poderia ser:
 
-// notas.forEach(somaNotas);
-//
-// function somaNotas (nota) {
-//    somaDasNotas += nota;
-// }
+// notas.forEach()(function (nota) { 
+//    somaDasNotas += nota;        
+// })
+
 
 const media = somaDasNotas / notas.length;
 
