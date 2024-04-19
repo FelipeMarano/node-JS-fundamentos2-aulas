afd567b87ffe3986db9643fb560ffd9595aa7225
Felipe Marano <felipehenriquemarano@gmail.com>
2024-04-19 10:45:53 -0300

    clonando arrays

---
+++ clonar.js
@@ -0,0 +1,6 @@
+const notas = [7, 7, 8, 9];
+const novaListaNotas = [...notas, 10]; // spread operator = para clonar arrays (se usa: ...) // 10 = para adicionar o que queremos
+                                      // como se fosse: novaListaNotas.push(10);
+
+console.log(notas);
+console.log(novaListaNotas);
